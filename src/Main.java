59590da4ed429461abf124f9d2e82888fc51fc49
Fitsum Seyoum <Fitsum.s11@gmail.com>
2024-01-28 18:57:00 -0500

    Test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArraysAndString one = new ArraysAndString();
+        int[] arr1 = new int[] {1,2,3,0,0,0};
+        int[] arr2 = new int[] {2, 5, 6};
+
+        one.merge(arr1, 3, arr2, 3);
+    }
+}
